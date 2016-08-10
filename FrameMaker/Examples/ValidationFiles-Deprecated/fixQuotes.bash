55033ec204e76753350a30d5c96bd5bd1350a3b8
R2 D2 <service_development@cip4.org>
2016-08-10 06:03:15 +0200

    Automated import from DropBox of Bob Herriot

--- FrameMaker/Examples/ValidationFiles-Deprecated/fixQuotes.bash
+++
@@ -1,6 +0,0 @@
-echo "find Strong Values"
-for foo in *.doc; do  
-echo $foo; 
-d:/'Program Files'/'Microsoft Office'/OFFICE11/Winword.exe /mFixQuotes $foo
-done
-
--- FrameMaker/Examples/ValidationFiles-Deprecated/word2txtU.bash
+++
@@ -1,7 +0,0 @@
-#for foo in $*; do 
-echo "save files as text"
-for foo in *.doc; do 
-echo $foo; 
-d:/'Program Files'/'Microsoft Office'/OFFICE11/Winword.exe /msaveTxt $foo
-done
-
